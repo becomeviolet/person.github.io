6e9555e38351270c12524c6efaafe1fb08e847da
becomeviolet <94172134+becomeviolet@users.noreply.github.com>
2023-06-04 12:48:37 +0800

    c

--- 临时.py
+++ 临时.py
@@ -3,8 +3,10 @@ from selenium.webdriver.common.by import By
 import time
 import csv
 
-dates = csv.reader(open("3.csv", "r"))
-
+dates = csv.reader(open("./3.csv", "r"))
+print(dates)
 for date in dates:
+    time.sleep(1)
     print(date)
+
 
---
+++ 临时2.py
@@ -0,0 +1,6 @@
+from selenium import webdriver
+import time
+driver = webdriver.Edge()
+
+driver.execute_script()
+print(123)
